a9a3070063f1b7202e86027f187397bc8b1555fe
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2023-12-31 16:44:03 +0530

    fixed categories

--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -2,28 +2,65 @@ import 'package:flutter/material.dart';
 
 import 'package:travelapp/res.dart';
 
-class CategoryItems extends StatelessWidget {
+class CategoryItems extends StatefulWidget {
   const CategoryItems({super.key});
 
   @override
+  State<CategoryItems> createState() => _CategoryItemsState();
+}
+
+class _CategoryItemsState extends State<CategoryItems> {
+  List<String> travelCategories = [
+    'Top Rated',
+    'Hotels',
+    'Flights',
+    'Restaurants',
+    'Activities',
+    'Landmarks',
+    'Beaches',
+    'Adventure',
+    'Cruises',
+    'Events',
+    'Shopping',
+    'Transportation',
+    'Culture',
+    'Nightlife',
+    'Nature',
+    'Family-Friendly',
+    'Luxury',
+    'Budget-Friendly',
+    'Historical',
+    'Local Cuisine',
+  ];
+
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    Widget category([bool activated = false]) {
+    Widget category(String text, int index, [bool activated = false]) {
       return Padding(
         padding: const EdgeInsets.only(right: 8),
-        child: Container(
-          height: 45,
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          decoration: BoxDecoration(
-              color: activated
-                  ? Constants.primaryColor
-                  : Constants.secondaryColorBg,
-              borderRadius: BorderRadius.circular(1000)),
-          child: Center(
-            child: Text(
-              "Top Rated",
-              style: TextStyle(
-                  color: activated ? Colors.white : Colors.black,
-                  fontWeight: FontWeight.w500),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          child: Container(
+            height: 45,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                color: activated
+                    ? Constants.primaryColor
+                    : Constants.secondaryColorBg,
+                borderRadius: BorderRadius.circular(1000)),
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                    color: activated ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.w500),
+              ),
             ),
           ),
         ),
@@ -34,11 +71,10 @@ class CategoryItems extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          category(true),
-          category(),
-          category(),
-          category(),
-          category(),
+          ...List.generate(
+              travelCategories.length,
+              (index) => category(
+                  travelCategories[index], index, selectedIndex == index))
         ],
       ),
     );
